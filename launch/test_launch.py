f59b885f7c3cdbd533bf624475a908c6d1e7cefc
Pranay Shirodkar <pranaykar@hotmail.com>
2022-07-05 13:23:24 +0800

    remove absolute path from launch file

--- launch/test_launch.py
+++ launch/test_launch.py
@@ -5,10 +5,10 @@ from launch.substitutions import EnvironmentVariable
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    # pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
     gazebo = launch.actions.ExecuteProcess(
             cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so',
-            '/home/osrc/dev_ws/src/rmf_camera/worlds/test.world'],
+            'install/rmf_camera/share/rmf_camera/worlds/test.world'],
             output='screen'
         )
     return LaunchDescription([
