917b44d284f16cab825b9ba53f0b31a6e4de6770
Pranay Shirodkar <pranaykar@hotmail.com>
2022-07-06 17:02:51 +0800

    initial pluginlib working

---
+++ include/HumanDetector.hpp
@@ -0,0 +1,30 @@
+#ifndef HUMANDETECTOR_HPP
+#define HUMANDETECTOR_HPP
+
+#include <rmf_obstacle_ros2/Detector.hpp>
+
+namespace rmf_human_detector {
+//==============================================================================
+class HumanDetector : public rmf_obstacle_ros2::Detector
+{
+public:
+  using Obstacles = rmf_obstacle_ros2::Detector::Obstacles;
+  using DetectorCallback = rmf_obstacle_ros2::Detector::DetectorCallback;
+
+  /// Documentation inherited
+  void initialize(
+    const rclcpp::Node& node,
+    DetectorCallback cb) final;
+
+  /// Documentation inherited
+  std::string name() const final;
+
+private:
+  DetectorCallback _cb;
+  std::string _name = "rmf_human_detector";
+
+};
+
+} // namespace rmf_human_detector
+
+#endif
---
+++ src/HumanDetector.cpp
@@ -0,0 +1,62 @@
+#include <rmf_obstacle_msgs/msg/obstacles.hpp>
+
+#include "HumanDetector.hpp"
+
+namespace rmf_human_detector {
+
+//==============================================================================
+void HumanDetector::initialize(
+  const rclcpp::Node& node,
+  DetectorCallback cb)
+{
+  _cb = std::move(cb);
+  RCLCPP_INFO(node.get_logger(), "Publishing: '%s'", "initialize end");
+
+
+  // detect all camera topics being published from gazebo world
+
+  // subscribe to camera topics
+  
+
+  // for each camera subscription
+    // initialize YoloDetector object, call imageCallback, return vector of obstacles
+    // convert obstacles from camera coordinates to world coordinates
+
+  // add all obstacles in rmf_obstacles::msg:Obstacles msg
+
+  // init obstacles_msg
+  auto obstacles_msg = rmf_obstacle_msgs::msg::Obstacles();
+  int num_obstacles = 4;
+  int id = 12;
+
+  // prepare obstacle_msg objects and add to obstacles_msg
+  obstacles_msg.obstacles.reserve(num_obstacles);
+  for (int i = 0; i < num_obstacles; i++) {
+    auto obstacle_msg = rmf_obstacle_msgs::msg::Obstacle();
+    // auto obstacle = rmf_obstacle_msgs::build<rmf_obstacle_msgs::msg::Obstacle>();
+    obstacle_msg.id = id++;
+    obstacle_msg.header.frame_id = "some stuff";
+    // obstacle.header.stamp = node.get_clock().now().to_msg();
+
+    obstacles_msg.obstacles.push_back(obstacle_msg);
+  }
+
+  //publish obstacles_msg
+  _cb(obstacles_msg);
+
+}
+
+//==============================================================================
+std::string HumanDetector::name() const
+{
+  return _name;
+}
+
+
+} // rmf_human_detector
+
+#include <pluginlib/class_list_macros.hpp>
+
+PLUGINLIB_EXPORT_CLASS(
+  rmf_human_detector::HumanDetector,
+  rmf_obstacle_ros2::Detector)
